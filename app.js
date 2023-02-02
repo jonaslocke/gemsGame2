59a671093214ae73cd96fa8192c81cbbd0e81f8f
Jonas Antunes <jonaslocke@gmail.com>
2023-02-02 11:39:16 -0300

    improvement

--- app.js
+++ app.js
@@ -30,8 +30,11 @@ const gemProps = {
 };
 
 class Gem {
-  constructor(color) {
+  constructor(color, index = 0) {
     this.color = this.getColor(color);
+    this.index = index;
+    this.selected = false;
+    this.highlighted = false;
   }
 
   getColor(color) {
@@ -44,22 +47,43 @@ class Gem {
   draw() {
     const gem = document.createElement("div");
     gem.classList.add(...["gem", this.color.toLowerCase()]);
+    if (this.selected) {
+      gem.classList.add("selected");
+    }
+    if (this.highlighted) {
+      gem.classList.add("highlighted");
+    }
+
+    gem.id = `gem-${this.index}`;
 
     return gem;
+  }
+  highlight(bool) {
+    this.highlighted = bool;
+  }
+  select(bool) {
+    this.selected = bool;
   }
 }
 class GameState {
   constructor(size = 4, anchor) {
+    this.size = clamp(size, 4, Math.floor(size / 4) * 4);
+    this.columns = this.getColumns();
     this.state = this.getState(size);
     this.anchor = this.getAnchor(anchor, size);
+    this.selectedGem = -1;
+    this.neighbors = null;
   }
-  getState(size) {
-    const max = Math.floor(size / 4) * 4;
-    size = clamp(size, 4, max);
-
-    return new Array(size)
+  getColumns() {
+    return this.size / 4;
+  }
+  getState() {
+    return new Array(this.size)
       .fill()
-      .map((_) => new Gem(getRandomItemFromArray(gemProps.getColors())));
+      .map(
+        (_, index) =>
+          new Gem(getRandomItemFromArray(gemProps.getColors()), index)
+      );
   }
 
   getAnchor(anchor, size) {
@@ -67,10 +91,62 @@ class GameState {
     return anchor;
   }
 
+  setSelectedGem(index) {
+    this.selectedGem = index;
+    this.state.forEach((gem, idx) => gem.select(index === idx));
+  }
+
+  findNeighbors() {
+    let top = this.selectedGem - this.columns;
+    let right = this.selectedGem + 1;
+    let bottom = this.selectedGem + this.columns;
+    let left = this.selectedGem - 1;
+
+    if (top < 0) {
+      top = null;
+    }
+    if (left < 0 || this.selectedGem % this.columns === 0) {
+      left = null;
+    }
+    if (right % this.columns === 0 || right > this.size) {
+      right = null;
+    }
+    if (bottom >= this.size) {
+      bottom = null;
+    }
+    this.neighbors = { top, right, bottom, left };
+    this.highlightNeighbors();
+  }
+
+  highlightNeighbors() {
+    const neighbors = Object.values(this.neighbors).filter(
+      (index) => index !== null
+    );
+
+    this.state.forEach((gem, index) =>
+      gem.highlight(neighbors.includes(index))
+    );
+  }
+
   draw() {
     this.anchor.innerHTML = "";
     for (let gem of this.state) {
-      this.anchor.appendChild(gem.draw());
+      const gemEl = gem.draw();
+      gemEl.addEventListener("click", () => {
+        const alreadySelected = this.selectedGem === gem.index;
+        if (alreadySelected) {
+          for (let gem of this.state) {
+            gem.select(false);
+            gem.highlight(false);
+            this.selectedGem = -1;
+          }
+        } else {
+          console.log({ selectedGem: gem.index });
+          this.setSelectedGem(gem.index);
+          this.findNeighbors();
+        }
+      });
+      this.anchor.appendChild(gemEl);
     }
   }
 }
@@ -80,6 +156,5 @@ const game = new GameState(16, gameEl);
 game.draw();
 
 setInterval(() => {
-  console.log("frameCount");
   game.draw();
-}, 1000);
+}, 300);
